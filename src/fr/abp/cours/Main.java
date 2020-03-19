fdaa8d260bdadddd6c9f0143fa65f47a281d265d
Anne <patesdegeek@gmail.com>
2020-03-19 18:36:44 +0100

    ajout des commentaires

--- src/fr/abp/cours/Main.java
+++ src/fr/abp/cours/Main.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        // 1.Déclaration des variables
         double unRayon;
         double lePerimetre;
-
-        System.out.println("Valeur du rayon :");
         Scanner lectureClavier = new Scanner(System.in);
+        //2.a Affichage du message "valeur du rayon" à l'écran
+        System.out.println("Valeur du rayon :");
+        //2.b Lecture au clavier d'une valeur, que l'on place dans la valeur unRayon
         unRayon = lectureClavier.nextDouble();
-
+        //3. Calcul de la circonférence
         lePerimetre = 2 * Math.PI * unRayon;
+        //4.Affichage du résultat
         System.out.println("Le cercle de rayon" + unRayon + "a pour périmètre:" + lePerimetre);
 
 
