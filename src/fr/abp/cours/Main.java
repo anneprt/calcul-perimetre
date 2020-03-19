1d60a841da6051a49e2bd29c7e62d7f46e298647
Anne <patesdegeek@gmail.com>
2020-03-19 15:46:53 +0100

    ajout saisie clavier et affichage du résultat

--- src/fr/abp/cours/Main.java
+++ src/fr/abp/cours/Main.java
@@ -1,5 +1,7 @@
 package fr.abp.cours;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,6 +10,12 @@ public class Main {
         double lePerimetre;
 
         System.out.println("Valeur du rayon :");
+        Scanner lectureClavier = new Scanner(System.in);
+        unRayon = lectureClavier.nextDouble();
+
+        lePerimetre = 2 * Math.PI * unRayon;
+        System.out.println("Le cercle de rayon" + unRayon + "a pour périmètre:" + lePerimetre);
+
 
     }
 }
